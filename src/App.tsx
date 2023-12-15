04c719ea5b11e704fa52591a793dac0b9a5a0b01
ChoiYeoJin <sonata7531@gmail.com>
2023-12-15 13:40:15 +0900

    글로벌 스타일 세팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
-import './index.css';
+import { GlobalStyles } from './styles/GlobalStyle';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Test from './pages/test/Test';
 import Home from './pages/home/Home';
 
 const App = () => (
   <>
+    <GlobalStyles />
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
