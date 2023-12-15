22407b7d981fe47930752752794cae61712d9979
raramen98 <swtraining98@gmail.com>
2023-12-15 15:20:41 +0900

    feat input

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,13 @@
-const Home = () => <div>Home oh my sweet Home</div>;
+import { TextInput, NumberInput } from '../../components/Input';
+
+const Home = () => {
+  return (
+    <>
+      <div>Home oh my sweet Home</div>
+      <TextInput></TextInput>
+      <NumberInput></NumberInput>
+    </>
+  );
+};
 
 export default Home;
