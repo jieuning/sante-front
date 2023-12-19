6b4fed6912c0f362a7426801b7ef022e0a1a483e
raramen98 <swtraining98@gmail.com>
2023-12-19 21:36:37 +0900

    feature header

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,5 @@
-// import React from 'react';
 import styled from 'styled-components';
 
-// ${props}에는 바뀌는 스타일
-// 활성화 시 컬러, 기본 컬러, 텍스트사이즈 등은 고정스타일
 const StyledInputWrapper = styled.div<{
   width?: string;
   height?: string;
@@ -26,12 +23,6 @@ const StyledInput = styled.input`
   }
 `;
 
-// 한 컴포넌트에 타입을 props로 (바꿔줄 수 있는 값)
-// 변수명은 bootstrap, MUI 참고
-// props: type, placeholder, value, onChange, style(width, height, text-align)
-// 디폴트값 필요한 데이터만 작성
-// value, onChange : 필수값
-
 interface StyledInputProps {
   type?: 'text' | 'number';
   placeholder: string;
@@ -41,7 +32,6 @@ interface StyledInputProps {
   textAlign: string;
 }
 
-// props 타입지정
 const Input = ({
   type = 'text',
   placeholder,
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,25 +1,34 @@
-import Input from '../../components/Input';
+// import Input from '../../components/Input';
+import Register from '../register';
 
 const Home = () => {
   return (
     <>
-      <div>Home oh my sweet Home</div>
-      <Input
-        type="text"
-        placeholder={'문자를 입력하세요.'}
-        width="300px"
-        height="40px"
-        textAlign="left"
-      />
-      <Input
-        type="number"
-        placeholder={'숫자를 입력하세요.'}
-        width="150px"
-        height="40px"
-        textAlign="center"
-      />
+      <Register></Register>
     </>
   );
 };
+
+// input 테스트
+// const Home = () => {
+//   return (
+//     <>
+//       <Input
+//         type="text"
+//         placeholder={'문자를 입력하세요.'}
+//         width="300px"
+//         height="40px"
+//         textAlign="left"
+//       />
+//       <Input
+//         type="number"
+//         placeholder={'숫자를 입력하세요.'}
+//         width="150px"
+//         height="40px"
+//         textAlign="center"
+//       />
+//     </>
+//   );
+// };
 
 export default Home;
