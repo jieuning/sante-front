3b187a89cd69a3de213bc7874bca79ce8b510cda
ChoiYeoJin <sonata7531@gmail.com>
2023-12-22 04:16:13 +0900

    통계 함수

--- src/pages/statistic/index.tsx
+++ src/pages/statistic/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useUserModel from '../../hooks/useUserModel';
 import {
   getMonthlyExerciseRateStatistic,
@@ -9,22 +9,44 @@ import Card from './Card';
 import styled from 'styled-components';
 
 //TODO - 여기서 백분율 계산해서 Card에 보내야함
+
+type ObjectType = {
+  list: number[];
+  result: number;
+};
 const Statistic = () => {
   const user = useUserModel(new Date('2023-12-01'), new Date('2023-12-10'));
+  const [exerciseRateList, setExerciseRateList] = useState<ObjectType>();
+  const [exerciseCntList, setExerciseCntList] = useState<ObjectType>();
+  const [caloryList, setCaloryList] = useState<ObjectType>();
   const arr = [100, 60, 30, 10, 50];
 
+  //여기서 비율 + 총 평균치 구해서 Card에 보내야함
   useEffect(() => {
-    const st = getMonthlyExerciseRateStatistic(
+    const rate = getMonthlyExerciseRateStatistic(
+      user?.userExerciseList,
+      new Date('2023-12-08'),
+      'rate'
+    );
+
+    if (rate) {
+      setExerciseRateList(rate);
+    }
+
+    const cnt = getMonthlyExerciseRateStatistic(
       user?.userExerciseList,
       new Date('2023-12-08'),
       'cnt'
     );
+    console.log(cnt?.list);
+    if (cnt) {
+      setExerciseCntList(cnt);
+    }
 
     const fst = getMonthlyCaloryTotalStatistic(
       user?.userFoodList,
       new Date('2023-12-08')
     );
-    console.log(fst);
   }, []);
 
   return (
@@ -32,11 +54,15 @@ const Statistic = () => {
       <ContentsContainer>
         <Title>🏃 운동 통계</Title>
         <CardContainer>
-          <Card title="운동 달성률" subTitle="이번달 평균 60%" data={arr} />
+          <Card
+            title="운동 달성률"
+            subTitle={`이번달 평균 ${exerciseRateList?.result ?? 0}%`}
+            data={exerciseRateList?.list ?? [0, 0, 0, 0, 0]}
+          />
           <Card
             title="운동 달성 횟수"
-            subTitle="이번달 총 운동 횟수 30회"
-            data={new Array(5).fill(60)}
+            subTitle={`이번달 총 운동 횟수 ${exerciseCntList?.result ?? 0}회`}
+            data={exerciseCntList?.list ?? [0, 0, 0, 0, 0]}
           />
         </CardContainer>
         <Title>🍚 식단 통계</Title>
@@ -50,6 +76,10 @@ const Statistic = () => {
       </ContentsContainer>
     </Container>
   );
+};
+
+const getAvg = (arr: number[]): number => {
+  return Math.ceil(arr.reduce((acc, curr) => acc + curr, 0) / arr.length);
 };
 
 const Container = styled.div`
