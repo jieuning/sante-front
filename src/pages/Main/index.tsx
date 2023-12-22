086971a53434ecd3c72f6b6616731fc0b3371767
ChoiYeoJin <sonata7531@gmail.com>
2023-12-22 10:46:50 +0900

    메인페이지 기초 구성

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,50 +3,68 @@ import RoutineCard from '../../components/RoutineCard';
 import { Exercise, Food } from '../../types/user';
 import styled from 'styled-components';
 import useUserModel from '../../hooks/useUserModel';
-
+import Header from '../../components/Header';
+import MainStatistic from '../../components/mainStatistic/MainStatistic';
 const TODAY = '2023-12-08';
+import { MonthCalendar } from '../../components/Calendar';
+import { DateSelect } from '../../components/DateSelect';
+
+interface BalckProps {
+  height?: string;
+}
 
 const Main = () => {
   const user = useUserModel(new Date(TODAY));
   console.log(user);
   return (
-    <Container>
-      <ContentsContainer>
-        <CardContainer>
-          <RoutineCard
-            type="exercise"
-            exerciseList={user?.userExerciseList}
-            isPlusIconVisible={true}
-            onClickMore={() => console.log('more click')}
-            onClickAdd={() => console.log('add click')}
-          ></RoutineCard>
-          <Blank />
-          <RoutineCard
-            type="food"
-            foodList={user?.userFoodList}
-            isPlusIconVisible={true}
-          ></RoutineCard>
-          <Blank />
-          <RoutineCard
-            type="exercise"
-            exerciseList={user?.userExerciseList}
-            date={new Date(TODAY)}
-          ></RoutineCard>
-        </CardContainer>
-      </ContentsContainer>
-    </Container>
+    <>
+      <Header />
+      <Blank height="6rem" />
+      <Container>
+        <DateSelect />
+        <Blank />
+        <ContentsContainer>
+          <MonthCalendar />
+          <CardContainer>
+            <RoutineCard
+              type="exercise"
+              exerciseList={user?.userExerciseList}
+              isPlusIconVisible={true}
+              onClickMore={() => console.log('more click')}
+              onClickAdd={() => console.log('add click')}
+            ></RoutineCard>
+            <Blank />
+            <RoutineCard
+              type="food"
+              foodList={user?.userFoodList}
+              isPlusIconVisible={true}
+            ></RoutineCard>
+            <Blank />
+            <RoutineCard
+              type="exercise"
+              exerciseList={user?.userExerciseList}
+              date={new Date(TODAY)}
+            ></RoutineCard>
+          </CardContainer>
+
+          <MainStatistic todayDate={new Date(TODAY)} />
+        </ContentsContainer>
+      </Container>
+    </>
   );
 };
 
 const Container = styled.div``;
 
-const Blank = styled.div`
-  height: 2rem;
+const Blank = styled.div<BalckProps>`
+  height: ${(props) => props.height || '2rem'};
 `;
 
 const ContentsContainer = styled.div`
   display: flex;
   justify-content: center;
+  gap: 3rem;
+  flex-wrap: wrap;
 `;
 
 const CardContainer = styled.div`
