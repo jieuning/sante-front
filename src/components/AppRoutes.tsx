5292126f952a34ec5eba707c6c22d53293a87a66
[SW7] 김민희 <whgh4100@gmail.com>
2023-12-22 16:20:05 +0900

    라우터

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -5,6 +5,7 @@ import Home from '../pages/home/Home';
 import Statistic from '../pages/Statistic';
 import Main from '../pages/Main';
 import Login from '../pages/login';
+import List from '../pages/List'
 const AppRoutes = () => (
   <Router>
     <GlobalStyles />
@@ -14,6 +15,7 @@ const AppRoutes = () => (
       <Route path="/statistic" element={<Statistic />} />
       <Route path="/main" element={<Main />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/list" element={<List />} />
     </Routes>
   </Router>
 );
