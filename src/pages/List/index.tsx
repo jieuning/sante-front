a07219fafaea62c7b7a12b6f49a72fa09ca5f754
[SW7] 김민희 <whgh4100@gmail.com>
2023-12-25 14:08:00 +0900

    Feat: 리스트페이지구현

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,3 +1,4 @@
+리스트페이지
 import Header from '../../components/Header';
 import { RadioButton, InputButtonInfo } from '../../components/RadioButton';
 import Arrow from '../../components/icons/Arrow';
@@ -5,14 +6,52 @@ import RoutineCard from '../../components/RoutineCard';
 import styled from 'styled-components';
 import { useState } from 'react';
 import useUserModel from '../../hooks/useUserModel';
-
-const TODAY = '2023-12-08';
+import { getMonth, getYear } from 'date-fns';
 
 const List = () => {
-  const [selectedValue, setSelectedValue] = useState('');
-  const today = new Date(TODAY); // 현재 날짜를 가져옵니다.
+  const today = new Date(); // 현재 날짜
+  const thisYear = getYear(today); // 현재 연도
+  const thisMonth = getMonth(today); // 현재 월
   const user = useUserModel(today);
 
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const [year, setYear] = useState(thisYear); // 현재 년도를 초기값으로
+  const [month, setMonth] = useState(thisMonth); // 현재 월을 초기값으로
+
+  //TODO - 운동/식단 리스트 받아오기
+  // 이전 달로 이동
+  const leftArrowHandler = () => {
+    if (month === 0) {
+      // 0은 1월로 1월에서 왼쪽 화살표를 누르면
+      setYear(year - 1); // 현재 년도에서 -1
+      setMonth(11); // 월도 12월로 설정
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  // 다음 달로 이동
+  const rightArrowHandler = () => {
+    if (month === 11) {
+      // 11은 12월로 12월에서 오른쪽 화살표를 누르면
+      setYear(year + 1); // 현재 년도에서 +1
+      setMonth(0); // 월도 1월로
+    } else {
+      setMonth(month + 1);
+    }
+  };
+  const filteredExerciseList = user?.userExerciseList.filter((exercise) => {
+    const exerciseDate = new Date(exercise.date);
+    return (
+      exerciseDate.getFullYear() === year && exerciseDate.getMonth() === month
+    );
+  });
+
+  const filteredFoodList = user?.userFoodList.filter((food) => {
+    const foodDate = new Date(food.date);
+    return foodDate.getFullYear() === year && foodDate.getMonth() === month;
+  });
   const radioCategoryButtonInfo: InputButtonInfo = {
     type: 'shortOvalRadio',
     size: 'short-oval',
@@ -25,11 +64,13 @@ const List = () => {
     onChange: (selectedCategory) => {
       console.log('선택된 값:', selectedCategory);
       setSelectedValue(selectedCategory);
-      // 선택된 아점저간에 따른 로직 수행
+      //TODO - 선택된 운동/식단에 따른 로직 작성
       if (selectedCategory === '운동') {
         console.log('운동');
+        console.log(filteredExerciseList);
       } else {
         console.log('식단');
+        console.log(filteredFoodList);
       }
     },
   };
@@ -43,22 +84,42 @@ const List = () => {
       </RadioBtnContainer>
 
       <WeeklyContainer>
-        <div>
+        <div onClick={leftArrowHandler}>
           <Arrow type="left" size="35" cursor="pointer"></Arrow>
         </div>
-        <DateBox>2023.12</DateBox>
-        <div>
+        <DateBox>{`${year}.${month + 1}`}</DateBox>
+        <div onClick={rightArrowHandler}>
           <Arrow type="right" size="35" cursor="pointer"></Arrow>
         </div>
       </WeeklyContainer>
 
       <RoutineCardContainer>
-        <RoutineCard
-          type="exercise"
-          exerciseList={user?.userExerciseList}
-          date={new Date(TODAY)}
-          onClickEdit={() => console.log('edit click')}
-        ></RoutineCard>
+        {selectedValue === '운동' && (
+          <>
+            {filteredExerciseList.map((exercise, index) => (
+              <RoutineCard
+                key={index}
+                type="exercise"
+                exerciseList={[exercise]}
+                date={new Date(exercise.date)}
+                onClickEdit={() => console.log('edit click')}
+              ></RoutineCard>
+            ))}
+          </>
+        )}
+        {selectedValue === '식단' && (
+          <>
+            {filteredFoodList.map((food, index) => (
+              <RoutineCard
+                key={index}
+                type="food"
+                foodList={[food]}
+                date={new Date(food.date)}
+                onClickEdit={() => console.log('edit click')}
+              ></RoutineCard>
+            ))}
+          </>
+        )}
       </RoutineCardContainer>
     </>
   );
