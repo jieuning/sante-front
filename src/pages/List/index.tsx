c64586029460360f6b76bfaedb8cb467664fb081
[SW7] 김민희 <whgh4100@gmail.com>
2023-12-25 23:45:18 +0900

    Feat: 리스트페이지 수정

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -3,30 +3,32 @@ import { RadioButton, InputButtonInfo } from '../../components/RadioButton';
 import RoutineCard from '../../components/RoutineCard';
 import styled from 'styled-components';
 import { useState } from 'react';
-import useUserModelAll from '../../hooks/useUserModelAll';  // 수정된 부분
+import useUserModel from '../../hooks/useUserModel'; // 수정된 부분
 import MonthlyDateSelector from '../../components/MonthlyDateSelector';
 import useMonthlyDateHandler from '../../hooks/useMonthlyDateHandler';
 
 const List = () => {
-  // 날짜 구하기
-  const today = new Date();
-  let firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
-  let lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-
-  // 날짜로 유저정보 받아오기, 해당 유저의 운동과 식단 가져오기
-  const user = useUserModelAll(firstDay, lastDay);  // 수정된 부분
-  
-  const exerciseList = user?.userExerciseList;
-  const foodList = user?.userFoodList;
-
-  // 스케줄드데이트의 배열
-  const exScheduledDate = exerciseList?.map((item) => item.scheduledDate);
-  const foodDate = foodList?.map((item) => item.foodId);
-
-  const [selectedValue, setSelectedValue] = useState('');
   const { targetDate, onLeftClick, onRightClick } = useMonthlyDateHandler(
     new Date()
   );
+
+  const user = useUserModel(
+    new Date(targetDate.getFullYear(), targetDate.getMonth(), 1),
+    new Date(targetDate.getFullYear(), targetDate.getMonth() + 1, 0)
+  );
+  console.log('user:', user);
+
+  const dateArray = [];
+  for (
+    let date = new Date(targetDate.getFullYear(), targetDate.getMonth(), 1);
+    date <= new Date(targetDate.getFullYear(), targetDate.getMonth() + 1, 0);
+    date.setDate(date.getDate() + 1)
+  ) {
+    dateArray.push(new Date(date));
+  }
+  console.log(dateArray);
+
+  const [selectedValue, setSelectedValue] = useState('');
 
   const radioCategoryButtonInfo: InputButtonInfo = {
     type: 'shortOvalRadio',
@@ -43,12 +45,8 @@ const List = () => {
       // TODO - 선택된 운동/식단에 따른 로직 작성
       if (selectedCategory === '운동') {
         console.log('운동');
-        console.log('해당월의운동리스트', exerciseList);
-        console.log('운동리스트의스케줄된날짜리스트', exScheduledDate);
       } else {
         console.log('식단');
-        console.log('해당월의식단리스트', foodList);
-        console.log('식단날짜리스트', foodDate);
       }
     },
   };
@@ -69,36 +67,36 @@ const List = () => {
         />
       </WeeklyContainer>
 
-      <RoutineCardContainer>
+      <AllRoutineCardContainer>
         {selectedValue === '운동' && (
           <>
-            <h2>운동 목록</h2>
-            {exerciseList?.map((exercise, index) => (
-              <RoutineCard
-                key={`exercise-${index}`}
-                type="exercise"
-                isMain={!true}
-                exerciseList={[exercise]}
-                date={targetDate} // 수정된 부분
-              ></RoutineCard>
+            {dateArray.map((date, index) => (
+              <RoutineCardContainer key={`exercise-${index}`}>
+                <RoutineCard
+                  key={`exercise-${index}`}
+                  type="exercise"
+                  exerciseList={user?.userExerciseList}
+                  date={date}
+                ></RoutineCard>
+              </RoutineCardContainer>
             ))}
           </>
         )}
         {selectedValue === '식단' && (
           <>
-            <h2>식단 목록</h2>
-            {foodList?.map((food, index) => (
-              <RoutineCard
-                key={`food-${index}`}
-                type="food"
-                isMain={!true}
-                foodList={[food]}
-                date={targetDate} // 수정된 부분
-              ></RoutineCard>
+            {dateArray.map((date, index) => (
+              <RoutineCardContainer key={`food-${index}`}>
+                <RoutineCard
+                  key={`food-${index}`}
+                  type="food"
+                  foodList={user?.userFoodList}
+                  date={date}
+                ></RoutineCard>
+              </RoutineCardContainer>
             ))}
           </>
         )}
-      </RoutineCardContainer>
+      </AllRoutineCardContainer>
     </>
   );
 };
@@ -116,6 +114,10 @@ const WeeklyContainer = styled.div`
 `;
 
 const RoutineCardContainer = styled.div`
+  margin: 10px 0;
+`;
+
+const AllRoutineCardContainer = styled.div`
   width: 50%;
   margin: auto;
 `;
