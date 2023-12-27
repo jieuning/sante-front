84206887ca61f04f27e1e3e06f0020e21cf72cf5
ChoiYeoJin <sonata7531@gmail.com>
2023-12-27 22:16:47 +0900

    모듈 테스트

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalStyles } from '../styles/GlobalStyle';
 import Test from '../pages/test/Test';
 import Home from '../pages/home/Home';
-import Statistic from '../pages/Statistic/';
+import Statistic from '../pages/Statistic/index.tsx';
 import Main from '../pages/Main/';
 import Login from '../pages/login/';
 import List from '../pages/List/';
