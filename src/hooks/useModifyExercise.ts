49319d5fb3195e2398cdc13b310adc4ca8212a5f
[SW7] 박소영 <soyo19585@gmail.com>
2023-12-28 17:52:48 +0900

    Exercise 관련 hook 상태받게 변경

--- src/hooks/useModifyExercise.ts
+++ src/hooks/useModifyExercise.ts
@@ -1,27 +1,28 @@
-import axios from 'axios';
 import { Exercise } from '../types/user';
 import { removeIdField } from '../utils/RemoveIdField';
-import { getEmail, getPassword } from '../utils/WebStorageControl';
-const URL = 'http://kdt-sw-7-team04.elicecoding.com/api/user';
+import { useStore } from '../states/user';
 
 interface ListProps {
   exercise: Exercise;
 }
 
 const useModifyExercise = () => {
+  const user = useStore((state) => state.user);
+  const getUser = useStore((state) => state.getUser);
+  const setUser = useStore((state) => state.setUser);
   const handleModify = async ({ exercise }: ListProps) => {
     try {
-      const response = await axios.post(`${URL}/check`, {
-        email: getEmail(),
-        password: getPassword(),
-      });
-      //업데이트를 위해 유저 전체 정보를 가져옴
-      const user = removeIdField(response.data.user);
-      delete user.__v;
+      getUser();
+
+      const filteredUser = removeIdField(user);
+      if (!filteredUser) {
+        return;
+      }
+      delete filteredUser.__v;
 
       const exerciseId = exercise.exerciseId;
 
-      user.userExerciseList?.forEach((exerciseData: Exercise) => {
+      filteredUser.userExerciseList?.forEach((exerciseData: Exercise) => {
         if (exerciseData.exerciseId === exerciseId) {
           console.log('---compare', exerciseData.exerciseId, '--', exerciseId);
           exerciseData.exerciseName =
@@ -40,25 +41,12 @@ const useModifyExercise = () => {
       });
 
       // 변경된 유저 그대로 업데이트
-      console.log('user', JSON.stringify(user));
+      console.log('user', JSON.stringify(filteredUser));
 
       // 서버에 변경 사항 업데이트
-      const putResponse = await axios.put(`${URL}`, JSON.stringify(user), {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (putResponse.status === 200) {
-        console.log('PUT 요청이 성공적으로 완료되었습니다.');
-      } else {
-        console.error(
-          'PUT 요청이 실패했습니다. HTTP 상태 코드:',
-          putResponse.status
-        );
-      }
+      setUser(filteredUser);
     } catch (error) {
-      console.error('checkboxHook error', error);
+      console.error('수정 요청 실패', error);
     }
   };
   return { handleModify };
